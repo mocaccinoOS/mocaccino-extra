2ba1b85c6defcd87d9faf743802d5797cf06bdba
Ettore Di Giacinto <mudler@gentoo.org>
2020-07-09 19:29:58 +0200

    Add mocaccino-skel

---
+++ packages/mocaccino-skel/mocaccino-init-skel.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ ! -e "/etc/fstab" ]; then
+	touch /etc/fstab
+fi
+
+if [ ! -e "/etc/passwd" ]; then
+	cp -rfv /etc/skel/etc/passwd /etc/passwd
+	echo "root:mocaccino" | chpasswd
+fi
+
+if [ ! -e "/etc/shadow" ]; then
+	cp -rfv /etc/skel/etc/shadow /etc/shadow
+fi
+
+if [ ! -e "/etc/hosts" ]; then
+	cp -rfv /etc/skel/etc/hosts /etc/hosts
+fi
+
+if [ ! -e "/etc/group" ]; then
+	cp -rfv /etc/skel/etc/group /etc/group
+fi
+
+if [ ! -d "/root" ]; then
+	mkdir /root
+fi
