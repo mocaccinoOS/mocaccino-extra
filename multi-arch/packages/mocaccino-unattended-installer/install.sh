04494d03b0eea054e6d9083637859c25e249d22f
Ettore Di Giacinto <mudler@sabayon.org>
2020-12-06 15:17:01 +0100

    Move luet_install where its actually used

--- multi-arch/packages/mocaccino-unattended-installer/install.sh
+++ multi-arch/packages/mocaccino-unattended-installer/install.sh
@@ -110,51 +110,6 @@ load_vars() {
   export INSTALL_DEVICE="${INSTALL_DEVICE:-$(yq r -j $file 'install_device')}"
 }
 
-luet_install() {
-  local rootfs=$1
-  local packages="$2"
-
-  ## Initial rootfs
-  pushd "$rootfs"
-
-  mkdir -p var/lock
-  mkdir -p run/lock
-  mkdir -p var/cache/luet
-  mkdir -p var/luet
-  mkdir -p etc/luet
-
-  mkdir -p dev
-  mkdir -p sys
-  mkdir -p proc
-  mkdir -p tmp
-  mkdir -p dev/pts
-  cp -rfv "${LUET_CONFIG}" etc/luet/luet.yaml
-  cp -rfv "${LUET_BIN}" luet
-
-  # Required to connect to remote repositories
-  if [ ! -f "etc/resolv.conf" ]; then
-    echo "nameserver 8.8.8.8" > etc/resolv.conf
-  fi
-  if [ ! -f "etc/ssl/certs/ca-certificates.crt" ]; then
-    mkdir -p etc/ssl/certs
-    cp -rfv "${CA_CERTIFICATES}" etc/ssl/certs
-  fi
-
-  if [ "${LUET_REPOS}" != "" ] ; then
-    ${SUDO} chroot . /luet install -y ${LUET_REPOS}
-  fi
-
-  ${SUDO} chroot . /luet config
-  ${SUDO} chroot . /luet install -y ${packages}
-  ${SUDO} chroot . /luet cleanup
-
-  # Cleanup/umount
-  umount_rootfs $rootfs || true
-
-  ${SUDO} rm -rf luet
-  popd
-}
-
 ### Main script
 
 export INSTALL_DEVICE="${INSTALL_DEVICE:-/dev/sda}"
--- multi-arch/packages/mocaccino-unattended-installer/scripts/copy.sh
+++ multi-arch/packages/mocaccino-unattended-installer/scripts/copy.sh
@@ -11,6 +11,51 @@ cat <<'EOF'
 
 EOF
 
+luet_install() {
+  local rootfs=$1
+  local packages="$2"
+
+  ## Initial rootfs
+  pushd "$rootfs"
+
+  mkdir -p var/lock
+  mkdir -p run/lock
+  mkdir -p var/cache/luet
+  mkdir -p var/luet
+  mkdir -p etc/luet
+
+  mkdir -p dev
+  mkdir -p sys
+  mkdir -p proc
+  mkdir -p tmp
+  mkdir -p dev/pts
+  cp -rfv "${LUET_CONFIG}" etc/luet/luet.yaml
+  cp -rfv "${LUET_BIN}" luet
+
+  # Required to connect to remote repositories
+  if [ ! -f "etc/resolv.conf" ]; then
+    echo "nameserver 8.8.8.8" > etc/resolv.conf
+  fi
+  if [ ! -f "etc/ssl/certs/ca-certificates.crt" ]; then
+    mkdir -p etc/ssl/certs
+    cp -rfv "${CA_CERTIFICATES}" etc/ssl/certs
+  fi
+
+  if [ "${LUET_REPOS}" != "" ] ; then
+    ${SUDO} chroot . /luet install -y ${LUET_REPOS}
+  fi
+
+  ${SUDO} chroot . /luet config
+  ${SUDO} chroot . /luet install -y ${packages}
+  ${SUDO} chroot . /luet cleanup
+
+  # Cleanup/umount
+  umount_rootfs $rootfs || true
+
+  ${SUDO} rm -rf luet
+  popd
+}
+
 export LUET_YES=true
 
 if [ -n "$ROOTFS_PACKAGES" ]; then
