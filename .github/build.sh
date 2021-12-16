b993204a26919640b70cfc4579f342827e2ee77d
Ettore Di Giacinto <mudler@sabayon.org>
2021-12-16 20:39:28 +0100

    ci: fixup pipelines for PR

--- .github/build.sh
+++ .github/build.sh
@@ -9,16 +9,24 @@ if [ -n "$PULL_REPOSITORY" ]; then
   PULL_ARGS="--pull-repository $PULL_REPOSITORY"
 fi
 
-if [ "$PUSH_CACHE" == "true" ]; then
-  sudo -E luet build $PULL_ARGS \
-          --only-target-package \
-          --values values/$ARCH.yaml --backend-args "--platform" --backend-args "linux/$ARCH" --backend-args "--load" \
-          --pull --push --image-repository $FINAL_REPO \
-          --no-spinner --live-output --tree packages "$1"
+if [ "$BUILDX" == "true" ]; then
+  if [ "$PUSH_CACHE" == "true" ]; then
+    sudo -E luet build $PULL_ARGS \
+            --only-target-package \
+            --values values/$ARCH.yaml --backend-args "--platform" --backend-args "linux/$ARCH" --backend-args "--load" \
+            --pull --push --image-repository $FINAL_REPO \
+            --no-spinner --live-output --tree packages "$1"
+  else
+    sudo -E luet build $PULL_ARGS \
+            --only-target-package \
+            --values values/$ARCH.yaml --backend-args "--platform" --backend-args "linux/$ARCH" --backend-args "--load" \
+            --pull --image-repository $FINAL_REPO \
+            --no-spinner --live-output --tree packages "$1"
+  fi
 else
   sudo -E luet build $PULL_ARGS \
           --only-target-package \
-          --values values/$ARCH.yaml --backend-args "--platform" --backend-args "linux/$ARCH" --backend-args "--load" \
+          --values values/$ARCH.yaml \
           --pull --image-repository $FINAL_REPO \
           --no-spinner --live-output --tree packages "$1"
 fi
