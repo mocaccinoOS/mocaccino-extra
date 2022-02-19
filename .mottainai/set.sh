ca626d2af476ca7c9a2c2e8b52265919a09d9d95
Ettore Di Giacinto <mudler@users.noreply.github.com>
2022-02-19 14:43:31 +0100

    Cleanup

--- .mottainai/set.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x
-mottainai-cli -p build webhook edit 1577128430104469230 filter refs/heads/master
-mottainai-cli -p build webhook edit 2387426890151640276 filter pull_request
-mottainai-cli -p build webhook update 2387426890151640276 task --yaml test-all.yaml
-mottainai-cli -p build webhook update 1577128430104469230 task --yaml build-all.yaml
-
